ab4136420d75453fb0df2e911d28d4e69fccdde7
Aniketh <anikethnair97@gmail.com>
2020-01-02 08:17:56 +0530

    Form Tutorial Complete

--- React/public/scripts/app.js
+++ React/public/scripts/app.js
@@ -8,60 +8,72 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-console.log("Input.js is running");
+console.log("Form.js is running");
 
-var ControlledInput = function (_React$Component) {
-  _inherits(ControlledInput, _React$Component);
+var MyForm = function (_React$Component) {
+  _inherits(MyForm, _React$Component);
 
-  function ControlledInput(props) {
-    _classCallCheck(this, ControlledInput);
+  function MyForm(props) {
+    _classCallCheck(this, MyForm);
 
-    var _this = _possibleConstructorReturn(this, (ControlledInput.__proto__ || Object.getPrototypeOf(ControlledInput)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (MyForm.__proto__ || Object.getPrototypeOf(MyForm)).call(this, props));
 
     _this.state = {
-      input: ''
+      input: '',
+      submit: ''
     };
-    // change code below this line
     _this.handleChange = _this.handleChange.bind(_this);
-    // change code above this line
+    _this.handleSubmit = _this.handleSubmit.bind(_this);
     return _this;
   }
-  // change code below this line
 
-
-  _createClass(ControlledInput, [{
+  _createClass(MyForm, [{
     key: 'handleChange',
     value: function handleChange(event) {
       this.setState({
         input: event.target.value
       });
     }
-    // change code above this line
-
+  }, {
+    key: 'handleSubmit',
+    value: function handleSubmit(event) {
+      // change code below this line
+      event.preventDefault();
+      this.setState(function (state) {
+        return {
+          submit: state.input
+        };
+      });
+      // change code above this line
+    }
   }, {
     key: 'render',
     value: function render() {
       return React.createElement(
         'div',
         null,
-        React.createElement('input', { value: this.state.input, onChange: this.handleChange }),
         React.createElement(
-          'h4',
-          null,
-          'Controlled Input:'
+          'form',
+          { onSubmit: this.handleSubmit },
+          React.createElement('input', { value: this.state.input, onChange: this.handleChange }),
+          React.createElement(
+            'button',
+            { type: 'submit' },
+            'Submit!'
+          )
         ),
         React.createElement(
-          'p',
+          'h1',
           null,
-          this.state.input
+          this.state.submit
         )
       );
     }
   }]);
 
-  return ControlledInput;
+  return MyForm;
 }(React.Component);
 
 ;
 
-ReactDOM.render(React.createElement(ControlledInput, null), document.getElementById('app'));
+ReactDOM.render(React.createElement(MyForm, null), document.getElementById('app'));
