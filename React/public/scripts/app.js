83fb508b7d1cb238c65c5a5c36bc7c5940832cd4
Aniketh <anikethnair97@gmail.com>
2020-01-02 09:59:06 +0530

    Magic Eight Ball Tutorial

--- React/public/scripts/app.js
+++ React/public/scripts/app.js
@@ -8,72 +8,84 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-console.log("Form.js is running");
+console.log("magic-8-ball.js is running");
 
-var MyForm = function (_React$Component) {
-  _inherits(MyForm, _React$Component);
+var inputStyle = {
+  width: 235,
+  margin: 5
+};
 
-  function MyForm(props) {
-    _classCallCheck(this, MyForm);
+var MagicEightBall = function (_React$Component) {
+  _inherits(MagicEightBall, _React$Component);
 
-    var _this = _possibleConstructorReturn(this, (MyForm.__proto__ || Object.getPrototypeOf(MyForm)).call(this, props));
+  function MagicEightBall(props) {
+    _classCallCheck(this, MagicEightBall);
+
+    var _this = _possibleConstructorReturn(this, (MagicEightBall.__proto__ || Object.getPrototypeOf(MagicEightBall)).call(this, props));
 
     _this.state = {
-      input: '',
-      submit: ''
+      userInput: '',
+      randomIndex: ''
     };
+    _this.ask = _this.ask.bind(_this);
     _this.handleChange = _this.handleChange.bind(_this);
-    _this.handleSubmit = _this.handleSubmit.bind(_this);
     return _this;
   }
 
-  _createClass(MyForm, [{
+  _createClass(MagicEightBall, [{
+    key: 'ask',
+    value: function ask() {
+      if (this.state.userInput) {
+        this.setState({
+          randomIndex: Math.floor(Math.random() * 20),
+          userInput: ''
+        });
+      }
+    }
+  }, {
     key: 'handleChange',
     value: function handleChange(event) {
       this.setState({
-        input: event.target.value
+        userInput: event.target.value
       });
-    }
-  }, {
-    key: 'handleSubmit',
-    value: function handleSubmit(event) {
-      // change code below this line
-      event.preventDefault();
-      this.setState(function (state) {
-        return {
-          submit: state.input
-        };
-      });
-      // change code above this line
     }
   }, {
     key: 'render',
     value: function render() {
+      var possibleAnswers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Most likely', 'Outlook not so good', 'Very doubtful'];
+      var answer = possibleAnswers[this.state.randomIndex]; // << change code here
       return React.createElement(
         'div',
         null,
+        React.createElement('input', {
+          type: 'text',
+          value: this.state.userInput,
+          onChange: this.handleChange,
+          style: inputStyle }),
+        React.createElement('br', null),
         React.createElement(
-          'form',
-          { onSubmit: this.handleSubmit },
-          React.createElement('input', { value: this.state.input, onChange: this.handleChange }),
-          React.createElement(
-            'button',
-            { type: 'submit' },
-            'Submit!'
-          )
+          'button',
+          { onClick: this.ask },
+          'Ask the Magic Eight Ball!'
+        ),
+        React.createElement('br', null),
+        React.createElement(
+          'h3',
+          null,
+          'Answer:'
         ),
         React.createElement(
-          'h1',
+          'p',
           null,
-          this.state.submit
+          answer
         )
       );
     }
   }]);
 
-  return MyForm;
+  return MagicEightBall;
 }(React.Component);
 
 ;
 
-ReactDOM.render(React.createElement(MyForm, null), document.getElementById('app'));
+ReactDOM.render(React.createElement(MagicEightBall, null), document.getElementById('app'));
